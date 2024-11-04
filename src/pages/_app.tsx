ec191455125951c0c3c50cb4c6b5b11abdfe1ed2
renata tsedenova <recedenova@gmail.com>
2024-11-04 17:15:43 +0400

    second commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
 import '../styles/globals.css';
-import axios from 'axios';
+// import axios from 'axios';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isHashValid, setIsHashValid] = useState(false);
+  // const [isHashValid, setIsHashValid] = useState(false);
 
-  useEffect(() => {
-    axios
-      .post('/api/validate-hash', { hash: window.Telegram.WebApp.initData })
-      .then((response) => setIsHashValid(response.status === 200));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .post('/api/validate-hash', { hash: window.Telegram.WebApp.initData })
+  //     .then((response) => setIsHashValid(response.status === 200));
+  // }, []);
 
-  if (!isHashValid) {
-    return null;
-  }
+  // if (!isHashValid) {
+  //   return null;
+  // }
   return <Component {...pageProps} />;
 }
 
